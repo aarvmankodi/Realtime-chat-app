f03f898c6491dbf2760b75280dd5e2770e0b9001
aryan jain <jainaryan631@gmail.com>
2024-06-23 15:57:39 +0530

    added user info

--- chat_app/src/components/chat_components/user.js
+++ chat_app/src/components/chat_components/user.js
@@ -1,129 +1,3 @@
-// import React , {useState , useRef , useEffect}from 'react'
-// import './user.css'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faEllipsisVertical, faUser, faBan} from '@fortawesome/free-solid-svg-icons'
-// import axios from 'axios';
-// import { toast } from 'react-toastify';
-
-// export default function User({talkingTo}) {
-//     const [menuOpen, setMenuOpen] = useState('close');
-//     const [grpMem , setGrpMem] = useState([]);
-//     const menuRef = useRef(null);
-
-//     const handleClickOutside = (event) => {
-//         if (menuRef.current && !menuRef.current.contains(event.target)) {
-//             setMenuOpen('close');
-//         }
-//     };
-
-//     useEffect(() => {
-//         document.addEventListener('mousedown', handleClickOutside);
-//         return () => {
-//             document.removeEventListener('mousedown', handleClickOutside);
-//         };
-//     }, []);
-
-//     const addUserToGrp = async () => {
-//       try{
-//         let user = prompt("add name of user to group?");
-//         console.log(user);
-
-//         let response = await axios.post("http://localhost:3001/addUserToGrp" , 
-//           {user , talkingTo} ,
-//           {withCredentials : true} )
-//           if (response.status === 200){
-//             console.log(response)
-//             console.log(`${user} added to ${talkingTo.chatter}`);
-//             toast.success(`${user} added to ${talkingTo.chatter}`);
-//             setGrpMem(response.data.members);
-//             console.log("gg" , grpMem);
-
-
-
-
-//           } else if (response.status === 210){
-//             toast.error("User does not exist");
-//           } else if (response.status === 218){
-//             toast.error("User already added to grp");
-//           }
-          
-//           else {
-//             console.log("response failed");
-//             console.log(response);
-//             toast.error("user not added");
-//           }
-//       }catch(e){
-//         console.log(e);
-//         toast.error("some error occured");
-//       }
-//     }
-
-//     const getGrpMem = async () => {
-//       try{
-//         let response = await axios.get('http://localhost:3001/grpMem' , 
-//           { params : {
-//             chatter : talkingTo.chatter} , 
-//           withCredentials : true});
-//         console.log(response.data.members);
-//         if (response.status === 200){
-//           setGrpMem(response.data.members);
-//           console.log("gg" , grpMem);
-//         }
-//       }catch(e){
-//         console.log(e);
-//       }
-//     }
-
-//     const getMembers = async () => {
-//       try{
-//         let response = await axios.get('http://localhost:3001/getMembers' , 
-//           {
-//             params : {
-//               chattingTo : talkingTo.chatter
-//             },
-//             withCredentials : true,
-//           });
-//           console.log(response);
-//           if (response.status === 200){
-//             console.log(response.data.name);
-//             console.log(response.data.email);
-//             console.log("success");
-//           }else {
-//             console.log("failure");
-//           }
-
-//       } catch(e){
-//         console.log(e);
-//       }
-//     }
-
-//     const toggleMenuOpen = () => {
-    
-//         if ( menuOpen === 'open')
-//             setMenuOpen('close');
-//         else
-//             setMenuOpen('open');
-//     }; 
-
-//     return (
-//       <div className={`user-info ${talkingTo.chatter == null ? `hide` : `nothing`}`}>
-//           <div className='user-icon'><FontAwesomeIcon icon={faUser}/></div>
-//           <div className='user-name'>{talkingTo.chatter}</div>
-//           <button id='toggle-chat-settings' onClick={toggleMenuOpen}><FontAwesomeIcon className="plus" icon={faEllipsisVertical} /></button>
-//           <div ref={menuRef} className={`chat-settings ${menuOpen}`}>
-//             <div className='chat-setting-button'>Delete Chats</div>
-//             <div className='chat-setting-button' onClick={getMembers}>Info</div>
-//             <div className='chat-setting-button' id='block'>Block </div>
-//             <div className={`chat-setting-button ${talkingTo.type == 'contact' ? 'hide' : 'nothing'}`} onClick={addUserToGrp}>Add Users</div>
-            
-//           </div>
-//       </div>
-//     )
-// }
-
-
-
-
 import React , {useState , useRef , useEffect}from 'react'
 import './user.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -133,14 +7,22 @@ import { toast } from 'react-toastify';
 
 export default function User({talkingTo , User}) {
     const [menuOpen, setMenuOpen] = useState('close');
+    const [email, setEmail] = useState('');
     const [grpMem , setGrpMem] = useState([]);
+    const [infoMenu , setInfoMenu] = useState('close');
     const menuRef = useRef(null);
+    const infoRef = useRef(null);
 
     const handleClickOutside = (event) => {
         if (menuRef.current && !menuRef.current.contains(event.target)) {
             setMenuOpen('close');
         }
     };
+    const handleClickOutsideInfo = (event) => {
+      if (infoRef.current && !infoRef.current.contains(event.target)) {
+          setInfoMenu('close');
+      }
+  };
 
     useEffect(() => {
         document.addEventListener('mousedown', handleClickOutside);
@@ -148,6 +30,13 @@ export default function User({talkingTo , User}) {
             document.removeEventListener('mousedown', handleClickOutside);
         };
     }, []);
+
+    useEffect(() => {
+      document.addEventListener('mousedown', handleClickOutsideInfo);
+      return () => {
+          document.removeEventListener('mousedown', handleClickOutsideInfo);
+      };
+  }, []);
 
     const addUserToGrp = async () => {
       try{
@@ -214,6 +103,7 @@ export default function User({talkingTo , User}) {
             console.log(response.data.name);
             console.log(response.data.email);
             console.log("success");
+            
           }else {
             console.log("failure");
           }
@@ -222,7 +112,13 @@ export default function User({talkingTo , User}) {
         console.log(e);
       }
     }
-
+    const toggleInfoMenuOpen = () => {
+    
+      if ( infoMenu === 'open')
+          setInfoMenu('close');
+      else
+          setInfoMenu('open');
+  };
     const toggleMenuOpen = () => {
     
         if ( menuOpen === 'open')
@@ -233,37 +129,66 @@ export default function User({talkingTo , User}) {
 
     const removeUser = async () => {
       const abc = window.confirm("are you sure?");
-      
-      try{
+      if (abc){
+        try{
         
-        const response = await axios.post("http://localhost:3001/removeUser" , 
-          {
-            user : User , 
-            talkingTo : talkingTo
-          },
-          {withCredentials : true});
-          console.log(response);
-          if (response.status === 200){
-            toast.success('Chat have been deleted');
-            window.location.reload();
-          } else {
-            toast.error("some failure has occured");
+          const response = await axios.post("http://localhost:3001/removeUser" , 
+            {
+              user : User , 
+              talkingTo : talkingTo
+            },
+            {withCredentials : true});
+            console.log(response);
+            if (response.status === 200){
+              toast.success('Chat have been deleted');
+              window.location.reload();
+            } else {
+              toast.error("some failure has occured");
+            }
+        }catch(e){
+          console.log(e);
+        }
+      } 
+    }
+    const getInfo = async () => {
+      try{
+        const res = await axios.get("http://localhost:3001/getInfo" , 
+          {withCredentials : true ,
+            params : {
+              user : talkingTo
+            }
           }
+        );
+        console.log(res);
+        if (res.status == 200){
+          
+          setEmail(res.data.email);
+          toggleInfoMenuOpen();
+        }
       }catch(e){
-        console.log(e);
+        console.log(e)
       }
     }
 
     return (
-      <div className={`user-info ${talkingTo.chatter == null ? `hide` : `nothing`}`}>
+      <div className={`user-info ${talkingTo.chatter == null ? 'hide' : 'nothing'}`}>
           <div className='user-icon'><FontAwesomeIcon icon={faUser}/></div>
           <div className='user-name'>{talkingTo.chatter}</div>
           <button id='toggle-chat-settings' onClick={toggleMenuOpen}><FontAwesomeIcon className="plus" icon={faEllipsisVertical} /></button>
           <div ref={menuRef} className={`chat-settings ${menuOpen}`}>
-            <div className='chat-setting-button' onClick={getMembers}>Info</div>
+            <div className='chat-setting-button' onClick={getInfo}>Info</div>
             <div className='chat-setting-button' id='block' onClick={removeUser}>Remove </div>
             <div className={`chat-setting-button ${talkingTo.type == 'contact' ? 'hide' : 'nothing'}`} onClick={addUserToGrp}>Add Users</div>
-            
+          </div>
+
+          <div ref={infoRef}className={`info-area chat-settings ${infoMenu}`}>
+          <div className='user-info-icon-container'>
+            <div className='user-info-icon'><FontAwesomeIcon icon={faUser}/></div>
+          </div>
+          
+          
+          <div className='user-name-info'>{talkingTo.chatter}</div>
+          <div className='user-email-info'>aarvmankodi06@gmail.com</div>
           </div>
       </div>
     )
--- socket_backend/info.js
+++ socket_backend/info.js
@@ -1,180 +1,3 @@
-// const express = require('express');
-// const mongoose = require("mongoose");
-// const cors = require('cors');
-// const User = require("./schemas/User");
-// const Message = require("./schemas/Message");
-// const Group = require("./schemas/Group");
-
-
-// const info = express.Router();
-
-// info.use(cors({
-//     origin: 'http://localhost:3000',
-//     credentials: true 
-// }));
-// info.use(express.json());
-
-// info.get('/user/contacts', async (req, res) => {
-//     if (req.session.user){
-//         const RealUser = await User.findOne({email : req.session.user.email});
-//         req.session.user.contacts = RealUser.contacts;
-//         req.session.user.groups = RealUser.groups;
-//         res.status(200).json({ contacts: req.session.user.contacts , groups : req.session.user.groups});
-//     } else {
-        
-//         console.log("user dont exist"); 
-//         res.status(201).json({ error: 'Not authenticated' }); 
-//     }
-// });
-
-// info.post('/user/contacts/add', async (req,res) => {
-//     ;
-//     try
-//         {
-//         const newUserName = req.body.newData.toLowerCase();
-//         const userName = req.session.user.name;
-            
-
-//         const user = await User.findOne({name : userName});
-//         const newUser = await User.findOne({name : newUserName});
-        
-//         if (user){
-//             if (newUser){
-//                 if (user.contacts.includes(newUser.name) || user.name === newUser.name){
-//                     return res.status(203).json({message : 'User is already in contacts'});
-//                 } else {
-//                     user.contacts.push(newUser.name);
-//                     req.session.user.contacts.push(newUser.name);
-//                     await user.save(); 
-//                     req.session.save();
-//                     console.log(req.session.user);
-//                     res.status(201).json({ message:'Data added successfully' });
-//                 }
-//             } else {
-//                 res.status(202).json({message : 'The user you are trying to add does not exist'});
-//             }
-//         }
-//     } catch (e){
-//         console.error('Error adding data:', e);
-//         res.status(500).json({ error: 'Something went wrong' });
-//     } 
-// })
-
-// info.post('/user/createGrp' , async (req,res) => {
-//     const grpName = req.body.grpName;
-//     const members = req.body.members;
-//     const userName = req.session.user.name;
-
-//     const user = await User.findOne({name : 
-//     userName});
-//     try{
-//         if (grpName && user){
-//             if (!Array.isArray(members)){
-//                 members = members.split(' ');
-//             }
-//             members.push(userName);
-//             const newGrp = new Group({
-//                 name : grpName,
-//                 participants : members,
-                
-//             })
-//             await newGrp.save();
-//             user.groups.push(grpName);
-//             await user.save();
-//             res.status(200).json({msg : "group created"});
-//         } else {
-            
-//             res.status(208).json({msg : "incorrect creds"});
-//         }
-//     }catch(e){
-//         res.status(218).json({msg : "an error occured"});
-//         console.log(e);
-//     }
-// })
-
-// info.get('/getCurrentUser', (req, res) => {
-//     if (req.session && req.session.user) {
-//       res.json({ username: req.session.user.name });
-//     } else {
-//       res.status(401).send('User not authenticated');
-//     }
-//   });
-
-// info.post('/addUserToGrp', async (req,res) => {
-        
-//     try {
-//         if (req.session.user){    
-//             const user = req.body.user;
-//             const talkingTo = req.body.talkingTo;
-//             console.log(user);
-//             console.log(talkingTo);
-//             if (talkingTo.type != 'group'){
-//                 res.status(404).json({err : "not a group"});
-//             } else {
-//                 const userInDb = await User.findOne({name : user});
-//                 const grpName = await Group.findOne({name : talkingTo.chatter});
-//                 if (!userInDb || !grpName){
-//                     res.status(210).json({message : "User or Group not found"});
-//                 }
-//                 else {
-//                     const userinGrp = await Group.findOne({
-//                         name : talkingTo.chatter , 
-//                         participants: { $elemMatch: { $eq: user }}
-//                     })
-//                     if (userinGrp){
-//                         res.status(218).json({message : "User already added"});
-//                     } else {
-//                         userInDb.groups.push(talkingTo.chatter);
-//                         await userInDb.save();
-//                         grpName.participants.push(user);
-//                         await grpName.save();
-//                         res.status(200).json({message : "User added" , members : grpName.participants});
-//                     }
-                
-//                 }
-                
-//             }}
-       
-//     } catch(e){
-//         console.log("the error when adding user to group is " , e);
-//         res.status(400).json({err : "an error occured in server"}); 
-//     }
-
-// })
-
-// info.get('/grpMem' , async (req,res) => {
-//     const grpName = req.query.chatter;
-//     try {
-//         const grp = await Group.findOne({name : grpName});
-//         if (grp){
-//             res.status(200).json({members : grp.participants});
-//         }
-//     }catch(e){
-//         console.log(e)
-//     }
-// })
-
-// info.get('/getMembers' , async (req,res) => {
-//     const sentName = req.query.chattingTo;
-
-//     const userFound = await User.findOne({name : sentName});
-
-//     if (userFound != null){
-//         const userEmail = userFound.email;
-//         res.status(200).json({
-//             message : "User was found. Here are the details",
-//             name : sentName,
-//             email : userEmail
-//         });
-//     } else {
-//         res.status(400).json({
-//             message : "User not found in db"
-//         });
-//     }
-// })
-// module.exports = info;
-
-
 const express = require('express');
 const mongoose = require("mongoose");
 const cors = require('cors');
@@ -276,6 +99,17 @@ info.get('/getCurrentUser', (req, res) => {
       res.status(401).send('User not authenticated');
     }
   });
+
+info.get('/getInfo', async (req, res) => {
+    const user = req.query.user.chatter;
+
+    const info = await User.findOne({name : user});
+    if (info) {
+        res.status(200).json({email : info.email});
+    }
+      
+    
+  })
 
 info.post('/addUserToGrp', async (req,res) => {
         
