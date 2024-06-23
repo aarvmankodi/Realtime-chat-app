31dc7d90eedd7181822eae64aec6367269f9bf4a
aryan jain <jainaryan631@gmail.com>
2024-06-23 15:53:26 +0530

    added block functionality

--- chat_app/src/components/Chat.js
+++ chat_app/src/components/Chat.js
@@ -1,3 +1,171 @@
+// import React, { useEffect, useState , useRef} from 'react';
+// import './chat.css';
+// import io from 'socket.io-client';
+// import axios from 'axios';
+// import Message from './chat_components/message';
+// import User from './chat_components/user';
+// import { toast } from 'react-toastify'; 
+
+// // Ensure withCredentials is set to true
+// const socket = io('http://localhost:3001', {
+//   withCredentials: true,
+// });
+
+// export default function Chat({ selectedChat }) {
+//   const clearMessages = () => {
+//     setMessages([]);
+//   };
+//   const [messages, setMessages] = useState([]);
+//   const [currentUser, setCurrentUser] = useState(null);
+//   const chatContainerRef = useRef(null);
+
+//   function linkify(text) {
+//     const urlPattern = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
+//     const wwwPattern = /(^|\s)(www\.[\S]+(\b|$))/gi;
+//     const noSchemePattern = /(^|\s)([A-Z0-9.-]+\.[A-Z]{2,}\b(?!@))/gi;
+  
+//     return text
+//       .replace(urlPattern, (url) => `<a href="${url}" target="_blank" rel="noopener noreferrer">${url}</a>`)
+//       .replace(wwwPattern, (match, prefix, url) => `${prefix}<a href="http://${url}" target="_blank" rel="noopener noreferrer">${url}</a>`)
+//       .replace(noSchemePattern, (match, prefix, url) => `${prefix}<a href="http://${url}" target="_blank" rel="noopener noreferrer">${url}</a>`);
+//   }
+
+  
+
+//   useEffect(() => {
+//     if (currentUser && selectedChat != {}) {
+//       socket.emit('join room', { currentUser, chatter : selectedChat.chatter }); 
+      
+//     }
+//   }, [selectedChat, currentUser]);
+  
+
+  
+//   useEffect(() => {
+//     socket.on('connect', () => {
+//       console.log('Connected to server');
+//       console.log(selectedChat);
+//       if (currentUser && selectedChat) {
+//         socket.emit('join room', { currentUser, chatter : selectedChat.chatter }); 
+//       }
+//     });
+  
+//     socket.on('connect_error', (error) => {
+//       console.error('Connection error:', error);
+//     });
+  
+//     socket.on('chat message', (msg) => {
+//       console.log("Received message: ????", msg);
+//       msg.msgs.forEach(message => {
+//         setMessages((prevMessages) => [...prevMessages ,{
+//           text : linkify(message.message) ,
+//           sender : message.sender
+//         }])
+//       })
+
+      
+  
+//     });
+  
+//     socket.on('clear chat', () => {
+//       console.log("Clearing chat");
+//       setMessages([]);
+//     });
+  
+//     return () => {
+//       socket.off('connect');
+//       socket.off('connect_error');
+//       socket.off('chat message');
+//     };
+//   }, [selectedChat]);
+ 
+
+//   useEffect(() => {
+//     const getMessages = () => {
+//       console.log("sending the parameter", selectedChat);
+//       axios.get('http://localhost:3001/getMsg', {
+//         withCredentials: true,
+//         params: { chatName: selectedChat.chatter }
+//       })
+//       .then(response => {
+//         console.log("Response received:", response);
+
+//         let msgs = response.data.messagesSent;
+//         let chatTo = response.data.chatTo;       
+        
+//         console.log("?????????????" , msgs);
+//         clearMessages();
+        
+//         handleSendMessage(msgs , chatTo);
+        
+//       })
+//       .catch(e => {
+//         console.error("Error while fetching messages:", e);
+//       });
+//     }
+
+//     if (selectedChat) {
+//       console.log("Selected chat:", selectedChat);
+//       clearMessages();
+//       getMessages();
+//     }
+//   }, [selectedChat]);
+
+//   useEffect(() => {
+//     const getCurrentUser = async () => {
+//       try {
+//         const response = await axios.get('http://localhost:3001/getCurrentUser', { withCredentials: true });
+//         console.log("the current user is " + response.data.username);
+//         setCurrentUser(response.data.username);
+//       } catch (error) {
+//         console.error('Failed to fetch current user:', error);
+//       }
+//     };
+
+//     getCurrentUser();
+//   }, []);
+  
+ 
+
+//   const handleSendMessage = (msgs, chatTo) => { 
+//     console.log("Sending message:", msgs);
+//     console.log("selected chat", chatTo);
+//     if (chatTo !== 'default'){
+      
+//       socket.emit('chat message', { chatTo, currentUser, msgs });
+//     } else{
+//       console.log("no user selected");
+//     }
+    
+//   };
+  
+//   return (
+//     <div className='chat'>
+//       <User talkingTo={selectedChat}/>
+//       <div className='outerchat' ref={chatContainerRef}>
+//       <ul className='list'>
+//         {messages.map((msg, index) => (
+//           <div className='chat-messages'>
+            
+//             <li 
+//                 className={`${msg.sender === currentUser ? 'self' : 'other'}`} 
+//                 dangerouslySetInnerHTML={{ __html: msg.text }}
+//               ></li>
+//            <li key={index+100}className={`sender ${msg.sender === currentUser ? 'self-user' : 'other-user'}`}>
+//            {msg.sender === currentUser ? 'you' : msg.sender}
+//             </li>
+//           </div>
+          
+//         ))}
+//       </ul>
+//       </div>
+      
+//       <Message onSendMessage={handleSendMessage}/>
+//     </div>
+//   );
+// }
+
+
 import React, { useEffect, useState , useRef} from 'react';
 import './chat.css';
 import io from 'socket.io-client';
@@ -20,7 +188,7 @@ export default function Chat({ selectedChat }) {
   const chatContainerRef = useRef(null);
 
   function linkify(text) {
-    const urlPattern = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
+    const urlPattern = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=_|!:,.;]*[-A-Z0-9+&@#\/%=_|])/gi;
     const wwwPattern = /(^|\s)(www\.[\S]+(\b|$))/gi;
     const noSchemePattern = /(^|\s)([A-Z0-9.-]+\.[A-Z]{2,}\b(?!@))/gi;
   
@@ -141,21 +309,23 @@ export default function Chat({ selectedChat }) {
   
   return (
     <div className='chat'>
-      <User talkingTo={selectedChat}/>
+      <User talkingTo={selectedChat} User={currentUser}/>
       <div className='outerchat' ref={chatContainerRef}>
       <ul className='list'>
         {messages.map((msg, index) => (
-          <div className='chat-messages'>
+          // <div className='chat-messages'>
             
-            <li 
-                className={`${msg.sender === currentUser ? 'self' : 'other'}`} 
-                dangerouslySetInnerHTML={{ __html: msg.text }}
-              ></li>
-           <li key={index+100}className={`sender ${msg.sender === currentUser ? 'self-user' : 'other-user'}`}>
-           {msg.sender === currentUser ? 'you' : msg.sender}
+          //   <li className={${msg.sender === currentUser ? 'self' : 'other'}} dangerouslySetInnerHTML={{ __html: msg.text }}></li>
+          //  <li key={index+100}className={sender ${msg.sender === currentUser ? 'self-user' : 'other-user'}}>
+          //  {msg.sender === currentUser ? 'you' : msg.sender}
+          //   </li>
+          // </div>
+          <div className='chat-messages'>
+            <li className={`${msg.sender === currentUser ? 'self' : 'other'}`} dangerouslySetInnerHTML={{ __html: msg.text }}></li>
+            <li key={index+100} className={`sender ${msg.sender === currentUser ? 'self-user' : 'other-user'}`}>
+            {msg.sender === currentUser ? 'you' : msg.sender}
             </li>
           </div>
-          
         ))}
       </ul>
       </div>
--- chat_app/src/components/chat_components/user.js
+++ chat_app/src/components/chat_components/user.js
@@ -1,3 +1,129 @@
+// import React , {useState , useRef , useEffect}from 'react'
+// import './user.css'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faEllipsisVertical, faUser, faBan} from '@fortawesome/free-solid-svg-icons'
+// import axios from 'axios';
+// import { toast } from 'react-toastify';
+
+// export default function User({talkingTo}) {
+//     const [menuOpen, setMenuOpen] = useState('close');
+//     const [grpMem , setGrpMem] = useState([]);
+//     const menuRef = useRef(null);
+
+//     const handleClickOutside = (event) => {
+//         if (menuRef.current && !menuRef.current.contains(event.target)) {
+//             setMenuOpen('close');
+//         }
+//     };
+
+//     useEffect(() => {
+//         document.addEventListener('mousedown', handleClickOutside);
+//         return () => {
+//             document.removeEventListener('mousedown', handleClickOutside);
+//         };
+//     }, []);
+
+//     const addUserToGrp = async () => {
+//       try{
+//         let user = prompt("add name of user to group?");
+//         console.log(user);
+
+//         let response = await axios.post("http://localhost:3001/addUserToGrp" , 
+//           {user , talkingTo} ,
+//           {withCredentials : true} )
+//           if (response.status === 200){
+//             console.log(response)
+//             console.log(`${user} added to ${talkingTo.chatter}`);
+//             toast.success(`${user} added to ${talkingTo.chatter}`);
+//             setGrpMem(response.data.members);
+//             console.log("gg" , grpMem);
+
+
+
+
+//           } else if (response.status === 210){
+//             toast.error("User does not exist");
+//           } else if (response.status === 218){
+//             toast.error("User already added to grp");
+//           }
+          
+//           else {
+//             console.log("response failed");
+//             console.log(response);
+//             toast.error("user not added");
+//           }
+//       }catch(e){
+//         console.log(e);
+//         toast.error("some error occured");
+//       }
+//     }
+
+//     const getGrpMem = async () => {
+//       try{
+//         let response = await axios.get('http://localhost:3001/grpMem' , 
+//           { params : {
+//             chatter : talkingTo.chatter} , 
+//           withCredentials : true});
+//         console.log(response.data.members);
+//         if (response.status === 200){
+//           setGrpMem(response.data.members);
+//           console.log("gg" , grpMem);
+//         }
+//       }catch(e){
+//         console.log(e);
+//       }
+//     }
+
+//     const getMembers = async () => {
+//       try{
+//         let response = await axios.get('http://localhost:3001/getMembers' , 
+//           {
+//             params : {
+//               chattingTo : talkingTo.chatter
+//             },
+//             withCredentials : true,
+//           });
+//           console.log(response);
+//           if (response.status === 200){
+//             console.log(response.data.name);
+//             console.log(response.data.email);
+//             console.log("success");
+//           }else {
+//             console.log("failure");
+//           }
+
+//       } catch(e){
+//         console.log(e);
+//       }
+//     }
+
+//     const toggleMenuOpen = () => {
+    
+//         if ( menuOpen === 'open')
+//             setMenuOpen('close');
+//         else
+//             setMenuOpen('open');
+//     }; 
+
+//     return (
+//       <div className={`user-info ${talkingTo.chatter == null ? `hide` : `nothing`}`}>
+//           <div className='user-icon'><FontAwesomeIcon icon={faUser}/></div>
+//           <div className='user-name'>{talkingTo.chatter}</div>
+//           <button id='toggle-chat-settings' onClick={toggleMenuOpen}><FontAwesomeIcon className="plus" icon={faEllipsisVertical} /></button>
+//           <div ref={menuRef} className={`chat-settings ${menuOpen}`}>
+//             <div className='chat-setting-button'>Delete Chats</div>
+//             <div className='chat-setting-button' onClick={getMembers}>Info</div>
+//             <div className='chat-setting-button' id='block'>Block </div>
+//             <div className={`chat-setting-button ${talkingTo.type == 'contact' ? 'hide' : 'nothing'}`} onClick={addUserToGrp}>Add Users</div>
+            
+//           </div>
+//       </div>
+//     )
+// }
+
+
+
+
 import React , {useState , useRef , useEffect}from 'react'
 import './user.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -5,7 +131,7 @@ import { faEllipsisVertical, faUser, faBan} from '@fortawesome/free-solid-svg-ic
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
-export default function User({talkingTo}) {
+export default function User({talkingTo , User}) {
     const [menuOpen, setMenuOpen] = useState('close');
     const [grpMem , setGrpMem] = useState([]);
     const menuRef = useRef(null);
@@ -105,15 +231,37 @@ export default function User({talkingTo}) {
             setMenuOpen('open');
     }; 
 
+    const removeUser = async () => {
+      const abc = window.confirm("are you sure?");
+      
+      try{
+        
+        const response = await axios.post("http://localhost:3001/removeUser" , 
+          {
+            user : User , 
+            talkingTo : talkingTo
+          },
+          {withCredentials : true});
+          console.log(response);
+          if (response.status === 200){
+            toast.success('Chat have been deleted');
+            window.location.reload();
+          } else {
+            toast.error("some failure has occured");
+          }
+      }catch(e){
+        console.log(e);
+      }
+    }
+
     return (
       <div className={`user-info ${talkingTo.chatter == null ? `hide` : `nothing`}`}>
           <div className='user-icon'><FontAwesomeIcon icon={faUser}/></div>
           <div className='user-name'>{talkingTo.chatter}</div>
           <button id='toggle-chat-settings' onClick={toggleMenuOpen}><FontAwesomeIcon className="plus" icon={faEllipsisVertical} /></button>
           <div ref={menuRef} className={`chat-settings ${menuOpen}`}>
-            <div className='chat-setting-button'>Delete Chats</div>
             <div className='chat-setting-button' onClick={getMembers}>Info</div>
-            <div className='chat-setting-button' id='block'>Block </div>
+            <div className='chat-setting-button' id='block' onClick={removeUser}>Remove </div>
             <div className={`chat-setting-button ${talkingTo.type == 'contact' ? 'hide' : 'nothing'}`} onClick={addUserToGrp}>Add Users</div>
             
           </div>
